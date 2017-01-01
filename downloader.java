d3c2c94994ebfacbc25fe79dd27325af6a74f86f
shikharsrivastava <shikharsri1996@gmail.com>
2017-01-01 10:16:04 +0530

    Added the first file downloader.java. Sets up multiple connection to download segments concurrently.

---
+++ downloader.java
@@ -0,0 +1,81 @@
+//23:29
+
+import java.util.*;
+import java.lang.*;
+import java.net.*;
+import java.io.*;
+
+
+class Th implements Runnable {
+
+	final int CHUNK = 401816;
+	Thread t;
+	String fileName;
+	int pos;
+	URLConnection uc;
+	public Th(String fileName,int pos,URLConnection uc) {
+		this.t = new Thread(this);
+		this.fileName =  fileName;
+		this.pos = pos;
+		this.uc = uc;
+		this.t.start();
+	}
+
+	public void run() {
+
+		try (RandomAccessFile f = new RandomAccessFile(fileName,"rw")) {
+			uc.setRequestProperty("Range","bytes=" + pos + "-");
+			System.out.println("pos = " + pos);
+			f.seek(pos);
+			
+			InputStream is = uc.getInputStream();
+			//BufferedInputStream is = new BufferedInputStream(inp,CHUNK);
+			int left  = CHUNK,ind = 0;
+			byte buff[] = new byte[CHUNK];
+			int rd,dwn = 0;
+			do {
+			rd = is.read(buff,ind,left);
+			if(rd != -1 && left > 0){
+			dwn += rd;
+			ind += rd;
+			left -= rd;
+			}
+			}
+			while(rd != -1 && left > 0);
+			f.write(buff,0,dwn);
+			System.out.println("Downloaded " + dwn);
+		}
+		catch (Exception e){
+			System.out.println(e);
+		}	
+	}
+	
+}
+
+class Downloader {
+
+	public static void main(String args[]) throws Exception {
+
+		final int CHUNK = 401816;
+		URL url = new URL(args[0]);
+		final String fileName = "Syllabus.pdf";
+
+		
+		URLConnection connection = url.openConnection();
+		
+		final long size = connection.getContentLength();
+		System.out.println(size);
+		Th tarr[] = new Th[1000];
+		int ind = 0, pos = 0;
+		 while(pos < size)
+		{
+			Th t = new Th(fileName,pos,url.openConnection());
+			tarr[ind++] = t;
+			pos += CHUNK;
+		}
+		
+		for(int i=0;i<ind;i++)
+			tarr[i].t.join();
+
+	}
+}
