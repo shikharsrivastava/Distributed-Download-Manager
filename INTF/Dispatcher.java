9dbf1b0acdfa9c0da6e23cbca5e2c2ab558579a0
shikharsrivastava <shikharsri1996@gmail.com>
2017-01-04 02:26:24 +0530

    It is the interface for Dispatcher class

---
+++ INTF/Dispatcher.java
@@ -0,0 +1,14 @@
+package INTF;
+
+/*
+	The dispatcher is the part that divides the large file among nodes
+
+	functions - 
+	getHostList() - returns an array of alive ip address running the server
+	Distribute() - Performs actual distribution among the nodes
+*/
+public interface Dispatcher {
+
+	String[] getHostList();
+	boolean Distribute();
+}
