92c4fac59e29a1b528663979d1a7fb724c2aff32
shikharsrivastava <shikharsri1996@gmail.com>
2017-01-04 11:04:18 +0530

    Added furthur actions

--- TRACKER/server.java
+++ TRACKER/server.java
@@ -10,7 +10,18 @@ class server {
 		try (ServerSocket s = new ServerSocket(TRACKER_PORT)) {
 		System.out.println("The tracker server is running on port " + port);
 
-		/* Accepting connection to take info*/
+		/*
+			Accepting connection to take info
+				Connect with every new user and save its information in a global object (Registration)
+				Make seperate thread for each client
+				Handle and exchange messages for success or faliure
+
+				Timely ping the all clients to check if someone has died
+				Make a Request object to manage a any sort of request from the corresponding client (Online User List, check if die loist,etc)
+								
+		*/
+
+
 
 	}
 
