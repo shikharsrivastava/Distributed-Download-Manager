701d9b9fff45affebf6cf06a9034c939778e6253
shikharsrivastava <shikharsri1996@gmail.com>
2017-01-06 22:42:25 +0530

    Did several bug fxes and improved design

--- TRACKER/server.java
+++ TRACKER/server.java
@@ -18,8 +18,11 @@ class Tracker {
 	synchronized public void refresh(PrintWriter os) {
 
 		for(String s: ipList) {
+			System.out.println("Written "+s);
 			os.println(s);
 		}
+		System.out.println("Written .");
+
 		os.println(".");
 	}
 
@@ -46,7 +49,7 @@ class Handler implements Runnable{
 		System.out.println(s);
 		// Adding this client to tracker list
 		clientIp = s.getInetAddress().getHostAddress();
-		this.tracker.add(clientIp);
+		//this.tracker.add(clientIp);
 		is = new BufferedReader(new InputStreamReader(s.getInputStream()));
 		os = new PrintWriter(s.getOutputStream(),true);
 	}
@@ -68,12 +71,13 @@ class Handler implements Runnable{
 			/*
 				Multiplex according to the request;
 			*/
-
-			if(request == "refresh") tracker.refresh(os);
-			else if(request == "disconnect") break;
-			else throw new Exception("Undefined request");
+			System.out.println("request == " + request);
+			if(request.equals("Server")) tracker.add(clientIp);
+			else if(request.equals("refresh")) tracker.refresh(os);
+			else if(request.equals("disconnect")) break;
+			else throw new Exception("Undefined request made");
 		}
-		
+
 	}
 		catch(Exception e) {
 			System.out.println(e);
@@ -99,7 +103,7 @@ class Handler implements Runnable{
 
 
 
-class server {
+class Server {
 
 	final static int TRACKER_PORT = 5000;
 	public static void main(String args[]) {
